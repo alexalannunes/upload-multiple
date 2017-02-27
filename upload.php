bbe1676b19d696f28980a75a342081cb9c625d1c
AlexAlanNunes <alexalannunes@hotmail.com>
2017-02-27 15:13:40 -0300

    nova estrura de nome - rand

--- upload.php
+++ upload.php
@@ -3,15 +3,22 @@
 	$i = 0;
 	#Analisa cada arquivo
 	foreach ($_FILES["arquivos"]["error"] as $key => $error) {
+
 	    # Definir o diretório onde salvar os arquivos.
-	    $destino = "files/" . $_FILES["arquivos"]["name"][$i];
+	    $destino = "files/" . rand(0, 0x3f3fff2) .'_'. $_FILES["arquivos"]["name"][$i] ;
 	   
 	    #Move o arquivo para o diretório de destino
-	    move_uploaded_file( $_FILES["arquivos"]["tmp_name"][$i], $destino );
+	    if(move_uploaded_file( $_FILES["arquivos"]["tmp_name"][$i], $destino )) {
+	    	echo 1;
+	    }
+	    else {
+	    	echo 0;
+	    }
 
 	    
 	    echo '<br>',$destino;
 	    #Próximo arquivo a ser analisado
 	    $i++;
+
 	}
 ?>
